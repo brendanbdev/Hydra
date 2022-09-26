8276639d63d37644769d6bbcb31fed2470fa4c44
brendanbdev <brendanbdev@gmail.com>
2022-09-26 13:01:33 -0400

    Create Hydra.bat

---
+++ Hydra.bat
@@ -0,0 +1,18 @@
+@echo off
+set count=0
+:loopstart
+cls
+color 0a
+set /a count+=1
+call :hydrafunc
+set /p attack=How do you attack?:
+if "%attack%" neq "do nothing" (goto :loopstart) else (goto :eof)
+
+:hydrafunc
+setlocal
+set hydraheads=0
+:createhydraheadloopstart
+call type "hydra head.txt"
+set /a hydraheads+=1
+if %hydraheads% lss %count% goto :createhydraheadloopstart
+endlocal
